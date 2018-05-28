85170a46bd9d86617f842d589d01af67aaf8b01f
André de Jager <andrethehunter@gmail.com>
2018-05-28 16:13:32 +1000

    Update usage

--- backpack.sh
+++ backpack.sh
@@ -13,7 +13,7 @@ ${txtbld}SYNOPSIS${txtrst}
 	${script_name} release
 	${script_name} snapshot [-l]
 	${script_name} test [-r]
-	${script_name} test-cljs [-r]
+	${script_name} test-cljs [-r|-n]
 
 ${txtbld}DESCRIPTION${txtrst}
 	${txtbld}clean${txtrst}
