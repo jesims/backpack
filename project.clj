f8adfb533b07eff909f764f3d3ab75f8472230cf
André de Jager <andrethehunter@gmail.com>
2018-06-04 12:25:43 +1000

    Remove snapshot suffix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.jesi/backpack "0.0.9-SNAPSHOT"
+(defproject io.jesi/backpack "0.0.9"
   :description "Clojure(Script) cross-project utilities"
   :license "Unlicensed"
   :url "https://github.com/jesims/backpack"
