796ef2f94aa97bdda972cc7f2f501d4d6f2f92ea
Alexander Scott <xander@cadcoder.com>
2018-10-09 19:53:26 +1000

    Update karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,29 @@
-module.exports = require('./build-scripts/karma.conf')
+// TODO Consider requiring as a dependency
+module.exports = config => config.set({
+  basePath: 'target/karma',
+  files: ['test.js'],
+  browsers: ['JesiChromiumHeadless'],
+  customLaunchers: {
+    JesiChromiumHeadless: {
+      base: 'ChromiumHeadless',
+      displayName: 'ChromiumHeadless',
+      flags: [
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--disk-cache-size=0',
+        '--headless',
+        '--no-sandbox' // TODO get rid of no-sandbox by not running as root in build-bus
+      ]
+    }
+  },
+  frameworks: ['cljs-test'],
+  plugins: ['karma-cljs-test', 'karma-chrome-launcher'],
+  colors: true,
+  logLevel: config.LOG_INFO,
+  client: {
+    args: ['shadow.test.karma.init'],
+    singleRun: true
+  },
+  singleRun: true,
+  retryLimit: 0
+})
