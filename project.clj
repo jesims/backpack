6afd5f8874825c155a874902ea5f7d7b89c2231b
André de Jager <andrethehunter@gmail.com>
2018-10-12 15:11:04 +1000

    WIP autodoc

--- project.clj
+++ project.clj
@@ -7,16 +7,18 @@
             :url          "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments     "same as Clojure"}
-  :plugins [[lein-parent "0.3.5"]]
+  :plugins [[lein-parent "0.3.5"]
+            [autodoc/lein-autodoc "1.1.1"]]
   :parent-project {:coords  [io.jesi/parent "0.0.1"]
                    :inherit [:plugins :managed-dependencies :deploy-repositories :dependencies :exclusions [:profiles :dev] :test-refresh]}
-  :dependencies [[org.clojure/clojurescript]
+  :dependencies [[org.clojure/clojure]
+                 [org.clojure/clojurescript]
                  [com.rpl/specter]
                  [thheller/shadow-cljs]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [medley "1.0.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
                  [pjstadig/humane-test-output "0.8.3"]]
+  :profiles {:test {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}}
   :clean-targets ^{:protect false} ["target"]
   :aot [io.jesi.backpack.random]
   :release-tasks [["deploy"]])
