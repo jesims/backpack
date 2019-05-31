a899578e502e6a5a819bdad507317cb9d5a42ea3
Alexander Scott <xander@cadcoder.com>
2019-05-31 15:09:18 +1000

    Remove java interface

--- project.clj
+++ project.clj
@@ -8,21 +8,20 @@
             :distribution :repo
             :comments     "same as Clojure"}
   :plugins [[lein-parent "0.3.5"]]
-  :parent-project {:coords  [io.jesi/parent "0.0.15"]
+  :parent-project {:coords  [io.jesi/parent "0.0.17"]
                    :inherit [:plugins :managed-dependencies :deploy-repositories :dependencies [:profiles :dev] :test-refresh]}
-  :dependencies [[thheller/shadow-cljs]
+  :dependencies [[thheller/shadow-cljs :scope "provided"]
                  [org.clojure/clojure "1.10.0"]
                  [org.clojure/core.async]
                  [com.rpl/specter]
                  [cheshire "5.8.1"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]
                  [medley "1.1.0"]]
-  :profiles {:test    {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}
+  :profiles {:test    {:dependencies [[org.clojure/tools.namespace "0.2.11"]]}}
              ;:dev     {:dependencies [;commented out until https://github.com/pjstadig/humane-test-output/issues/37 is fixed
              ;                         [pjstadig/humane-test-output "0.9.0"]]
              ;          ;commented out until https://github.com/weavejester/codox/issues/166 is fixed
              ;          :plugins      [[lein-codox "0.10.6"]]}
-             :install {:aot [io.jesi.backpack.random]}}
   :clean-targets ^{:protect false} ["target"]
   :release-tasks [["deploy"]]
   ;FIXME generate docs for cljc, clj and cljs
